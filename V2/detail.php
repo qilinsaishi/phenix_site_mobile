e47c1ab1a296e3f4a48e1b751f5ff666780ed749
justin.chen <cxd032404@hotmail.com>
2021-06-01 12:15:40 +0800

    merge

--- V2/detail.php
+++ V2/detail.php
@@ -29,7 +29,7 @@ if(isset($return["information"]['data']['redirect']) && $return["information"]['
             <img src="<?php echo $config['site_url'];?>/images/logo.png" alt="" class="logo_img">
             <span class="logo_title">凤凰电竞</span>
             <i class="dot"></i>
-            <span class="logo_title"><?php echo $return['information']['data']['title'];?></span>
+            <span class="logo_title"></span>
         </div>
         <div id='menu'>
             <img src="<?php echo $config['site_url'];?>/images/menu.png" alt="">
